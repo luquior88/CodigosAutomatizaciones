f3975aec142d6c7fed1a86de94f15a0aeffd9d33
luquior88 <luquio88@gmail.com>
2024-08-07 15:08:22 -0300

    Create BajadaBasesDeDatos.py
    Codigo para bajar info de base de datos con python

---
+++ BajadaBasesDeDatos.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import sqlalchemy
+import os
+
+# arriba importo librerias 
+
+# llamo clase de manejo de errores
+from ManejadorError import mailError
+
+# clase:
+def queryDATOS():
+  # Defino variables con datos guardados en windows
+	usuario = os.environ.get('Usuario')
+	contrasenia = os.environ.get('PassNew')
+
+  # Conecto con la base de datos:
+	engine = sqlalchemy.create_engine('mysql+pymysql://'+usuario+':'+contrasenia+'@BaseDeDatos')
+
+  # Genero la variable query con la query a ser ejecutada:
+	query = ''' 
+
+	SELECT *
+	FROM Base de datos
+  WHERE Condiciones
+
+	'''
+
+  
+	df = pd.read_sql_query(query,engine)
+
+  # Exporto a csv
+	export_csv = df.to_csv (r"E:/3- Reportes/accesos.csv", index = None, header=True)
+
+
+try:
+	queryDATOS()
+except:
+	mailError("Se genero un error en la bajada de Accesos")
